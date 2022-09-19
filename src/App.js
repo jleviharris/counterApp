c67cf48f3d68835295c5d601c64643b12ba7a717
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-09-19 14:33:08 -0400

    Initial Commit, day one of 100 days of javascript complete

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const subtract = () => {
+    setCounter(counter - 1);
+  };
+  const add = () => {
+    setCounter(counter + 1);
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <div className="counter">{counter}</div>
+
+      <br />
+      <ul>
+        <li>
+          <button className="subtractBttn" onClick={subtract}>
+            SUBTRACT
+          </button>
+        </li>
+        <li>
+          <button className="resetBttn" onClick={reset}>
+            RESET
+          </button>
+        </li>
+        <li>
+          <button className="addBttn" onClick={add}>
+            ADD
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
